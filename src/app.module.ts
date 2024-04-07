1115ab7da34c239663e92a64f85442f4edab26b9
Csaba Szabo <szabo_csaba96@outlook.com>
2024-04-07 09:38:05 +0200

    stock module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { StockModule } from './stock/stock.module';
 
 @Module({
   imports: [
+    StockModule,
     ScheduleModule.forRoot()
   ],
   controllers: [],
